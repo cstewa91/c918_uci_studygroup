2c30200ca4f642ef629142493d2280afd1ab273e
Collin Stewart <cstewa91@gmail.com>
2018-11-15 16:43:00 -0800

    Initial Commit

---
+++ client/src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/css/app.css';
+import logo from '../assets/images/logo.svg';
+
+const App = () => (
+    <div>
+        <div className="app">
+            <h1>Welcome to react</h1>
+        </div>
+    </div>
+);
+
+export default App;
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const { resolve } = require('path');
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
+
+app.get('*', (req, res) => {
+   res.sendFile(resolve(__dirname, 'client', 'dist,', 'index.html'));
+});
+
+app.listen(PORT, () => {
+   console.log('Server running on PORT: ' + PORT);
+});
