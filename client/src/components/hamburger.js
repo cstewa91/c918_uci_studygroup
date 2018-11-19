ffbbb4f29fce5f8b59859ededa1904becddc7d11
Collin Stewart <cstewa91@gmail.com>
2018-11-18 23:44:41 -0800

    Added any routing that was missing from each component

--- client/src/components/hamburger.js
+++ client/src/components/hamburger.js
@@ -12,17 +12,14 @@ export default ()=>{
                 <Link to='/profile' className='btn blue confirm'>Profile</Link>
             </div>
             <div className='hamburger-search'>
-                <Link to='/search' className='btn blue confirm'>Search</Link>
+                <Link to='/search-group' className='btn blue confirm'>Search</Link>
             </div>
             <div className='hamburger-create'>
-                <Link to='/create' className='btn blue confirm'>Create</Link>
+                <Link to='/create-group' className='btn blue confirm'>Create</Link>
             </div>
             <div className='hamburger-created'>
                 <Link to='/created-group' className='btn blue confirm'>Created Groups</Link>
-            </div>
-            <div className='hamburger-landing'>
-                <Link to='/' className='btn blue confirm'><button className='btn blue logout'>Log Out</button></Link>
-            </div>  
+            </div> 
         </div>
     )
 }
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -6,11 +6,12 @@ export default () => {
    return (
       <div>
          <h1 className="home">Home Page</h1>
+         <Link to="/hamburger">Hamburger</Link>
          <Link to="/search-group"><button className="float">Search</button></Link>
          <div className="moreCss">Joined</div>
          <div className="moreCss">Created</div>
          <Link to="/create-group"><button className="float">Create a group</button></Link>
-         <Link to="/awesome-group"><div className="moreCss">Awesome Group</div></Link>
+         <Link to="/selected-group"><div className="moreCss">Awesome Group</div></Link>
       </div>
    )
 }
