a3bec78b091d82a5c9f5cd3cbdf2e065ee087413
Hapachino <erickwbrownfield@gmail.com>
2018-11-23 19:50:00 -0800

    dbconfig default

--- backend/config/studygroupfinder.sql
+++ backend/config/studygroupfinder.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 24, 2018 at 01:51 AM
+-- Generation Time: Nov 24, 2018 at 03:47 AM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.0.13
 
@@ -31,8 +31,8 @@ USE `studygroupfinder`;
 --
 
 CREATE TABLE IF NOT EXISTS `groups` (
-  `id` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
+  `id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
   `name` varchar(40) NOT NULL,
   `location` varchar(40) NOT NULL,
   `subject` varchar(30) NOT NULL,
@@ -67,9 +67,9 @@ INSERT INTO `groups` (`id`, `user_id`, `name`, `location`, `subject`, `course`, 
 
 CREATE TABLE IF NOT EXISTS `group_members` (
   `id` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+  `group_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `group_members`
@@ -77,11 +77,10 @@ CREATE TABLE IF NOT EXISTS `group_members` (
 
 INSERT INTO `group_members` (`id`, `group_id`, `user_id`) VALUES
 (1, 1, 1),
-(2, 2, 1),
 (3, 1, 3),
+(2, 2, 1),
 (6, 2, 3),
 (7, 3, 1),
-(11, 1, 29),
 (32, 16, 31);
 
 -- --------------------------------------------------------
@@ -93,20 +92,15 @@ INSERT INTO `group_members` (`id`, `group_id`, `user_id`) VALUES
 CREATE TABLE IF NOT EXISTS `sessions` (
   `id` int(10) unsigned NOT NULL,
   `token` varchar(20) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+  `user_id` int(11) unsigned NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `sessions`
 --
 
 INSERT INTO `sessions` (`id`, `token`, `user_id`) VALUES
-(4, 'novu3loye2', 1),
-(5, 'rwu0aop8tlm', 19),
-(10, 'nm2srmicpk', 29),
-(19, '942pbdllew', 44),
-(21, 'citp58l33', 47),
-(22, '2fy40xypa5w', 31);
+(29, 'wjkv4zbocyq', 31);
 
 -- --------------------------------------------------------
 
@@ -115,14 +109,14 @@ INSERT INTO `sessions` (`id`, `token`, `user_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(11) unsigned NOT NULL,
   `google_id` varchar(30) NOT NULL,
   `username` varchar(100) NOT NULL,
   `firstname` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL,
   `email` varchar(40) NOT NULL,
   `password` varchar(40) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
@@ -132,7 +126,6 @@ INSERT INTO `users` (`id`, `google_id`, `username`, `firstname`, `lastname`, `em
 (1, 'a1b2', 'johndoe2000', 'john', 'doe', 'johndoe@gmail.com', '123'),
 (3, 'd5a5adkj', 'janedoe', 'jane', 'doe', 'janedoe@gmail.com', ''),
 (20, 'abcd', 'Hapachino', 'Hapa', 'Chino', 'lfz@gmail.com', ''),
-(29, 'a', 'b', 'king', 'james', 'kingjames@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef'),
 (30, 'a', 'b', 'king', 'james', 'gg@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef'),
 (31, 'test', 'hellokitty', 'test', 'test', 'test@gmail.com', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
 
@@ -150,21 +143,26 @@ ALTER TABLE `groups`
 -- Indexes for table `group_members`
 --
 ALTER TABLE `group_members`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `group_id_2` (`group_id`,`user_id`),
+  ADD KEY `group_id` (`group_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `sessions`
 --
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_id` (`user_id`);
+  ADD UNIQUE KEY `user_id` (`user_id`),
+  ADD UNIQUE KEY `token` (`token`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -174,22 +172,39 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `group_members`
 --
 ALTER TABLE `group_members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
 --
 -- AUTO_INCREMENT for table `sessions`
 --
 ALTER TABLE `sessions`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
+  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `group_members`
+--
+ALTER TABLE `group_members`
+  ADD CONSTRAINT `group_members_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `group_members_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
