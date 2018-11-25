c2e34a62fb9108dbe576851e24ecb26dbba5379f
Calberk <park.edmund@gmail.com>
2018-11-25 01:59:18 -0800

    adjusted profile/edit/hamburger and header search link

--- client/src/components/general/hamburger.js
+++ client/src/components/general/hamburger.js
@@ -9,17 +9,14 @@ export default () => {
             <div className='hamburger-home'>
                 <Link to='/home' className='btn blue confirm'>Home</Link>
             </div>
-            <div className='hamburger-profile'>
-                <Link to='/profile' className='btn blue confirm'>Profile</Link>
-            </div>
             <div className='hamburger-search'>
                 <Link to='/search-group' className='btn blue confirm'>Search</Link>
             </div>
             <div className='hamburger-create'>
                 <Link to='/create-group' className='btn blue confirm'>Create</Link>
             </div>
-            <div className='hamburger-created'>
-                <Link to='/created-group' className='btn blue confirm'>Created Groups</Link>
+            <div className='hamburger-profile'>
+                <Link to='/profile' className='btn blue confirm'>Profile</Link>
             </div>
         </div>
     )
--- client/src/components/general/header.js
+++ client/src/components/general/header.js
@@ -8,23 +8,26 @@ import './header.css';
 export default () => {
     return (
         <div className='header-container'>
-            <nav className='navbar'>
-                <div className='nav-header row d-flex align-items-center'>
-                    <div className='hamburger'>
-                        <img className='burger-bar' src={menu} />
-                    </div>
-                    <div className='nav-logo col-2'>
-                        <img className='worm' src={worm} />
-                    </div>
-                    <div className='nav-title col-7'>
-                        <span className='navbar-text'>Book Worms</span>
-                    </div>
-                    <div className="nav-search">
-                        <img src={magnifier} className="search-icon" />
-                    </div>
+        <nav className='navbar'>
+            <div className='nav-header row d-flex align-items-center'>
+                <div className='hamburger'>
+                    <img className='burger-bar' src={menu}/>
                 </div>
-            </nav>
-        </div>
+                <div className='nav-logo col-2'>
+                    <img className='worm' src={worm}/>
+                </div>
+                <div className='nav-title col-7'>
+                    <span className='navbar-text'>Book Worms</span>
+                </div>
+                <div className="nav-search">
+                    <Link to='/search-group'>
+                        <img src={magnifier} className="search-icon"></img>
+                    </Link>
+                    
+                </div>
+            </div>
+        </nav>  
+    </div>
     )
 }
 
