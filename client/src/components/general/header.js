9cd425232dae56c0a1d891fb22d64e6b0dc3c40c
Calberk <park.edmund@gmail.com>
2018-12-06 23:57:31 -0800

    added media queries to header component

--- client/src/components/general/header.js
+++ client/src/components/general/header.js
@@ -20,52 +20,51 @@ class Header extends Component {
 
         if(auth){
             return (
-                <div className='header-signout d-flex flex-row-reverse'>
                     <button onClick={userSignOut} className='btn header-signout-button '>Log Out</button>
-                </div>
             )
         }
     }
 
     render(){
         return (
-            <div className=' header-container'>
-                    <nav className=' nav navbar row justify-content-between '>
-                        <div className='nav-hamburger-button'>
-                            <HamburgerButton click={this.props.hamburgerClick}/>
-                        </div>
-                        <div className='nav-title nav-brand'>
-                            <Link to='/home'>
-                                <img className='worm nav-logo' src={worm}/>
-                                <span className='navbar-text'>Book Worms</span>
-                            </Link>
-                            {this.SignOutButton()}
-                        </div>
+            <div className='header-container container-fluid'>
+                <nav className='header-navbar row justify-content-between align-items-center'>
+                    <div className='nav-hamburger-button'>
+                        <HamburgerButton click={this.props.hamburgerClick}/>
+                    </div>
+                    <div className='nav-title'>
+                        <Link to='/home'>
+                            <img className='worm nav-logo' src={worm}/>
+                            <span className='navbar-text'>Book Worms</span>
+                        </Link>
                         
-                        <div className="nav-search">
-                            <Link to='/search-group'>
-                                <img src={magnifier} className="search-icon"></img>
-                            </Link>
-                        </div>
-                        <div className="nav-links row ">
-                            <ul className="d-flex col-12 justify-content-around align-items-end">
-                                <li>
-                                    <Link to='/home'>HOME</Link>
-                                </li>
-                                <li>
-                                    <Link to='/search-group'>SEARCH</Link>
-                                </li>
-                                <li>
-                                    <Link to='/create-group'>CREATE</Link>
-                                </li>
-                                <li>
-                                    <Link to='/profile'>PROFILE</Link>
-                                </li>
-                            </ul>
-                            
-                        </div>
-                    </nav>
-                    
+                    </div>
+                    <div className="nav-search">
+                        <Link to='/search-group'>
+                            <img src={magnifier} className="search-icon"></img>
+                        </Link>
+                    </div>
+                    <div className="nav-links row justify-content-center">
+                        <ul className="d-flex align-items-center">
+                            <li>
+                                <Link to='/home'>HOME</Link>
+                            </li>
+                            <li>
+                                <Link to='/search-group'>SEARCH</Link>
+                            </li>
+                            <li>
+                                <Link to='/create-group'>CREATE</Link>
+                            </li>
+                            <li>
+                                <Link to='/profile'>PROFILE</Link>
+                            </li>
+                            <li>
+                            {this.SignOutButton()}  
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </nav>                    
         </div>
         )
 
