c22373a7bfaab6513c561f92ab19109bff447b7b
Collin <cstewa91@gmail.com>
2018-12-07 10:13:23 -0800

    Created floating text for all input fields

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './login/login.css'
 
-export default ({ input, label, meta: { error, touched }, size, type = 'text' }) => (
-   <div>
-      <input {...input} type={type} placeholder={label} />
-      <p>{touched && error}</p>
-   </div>
-)
+class Input extends Component {
+   state = {
+      activeField: false
+
+   }
+   landInputLabel = (event) => {
+      if (event.target.value === "") {
+         this.setState({
+            activeField: false,
+         })
+      }
+   }
+   floatInputLabel = () => {
+      this.setState({
+         activeField: true
+      });
+   }
+   render() {
+      const { input, label, className = 'user-input', meta: { error, touched }, size, type = 'text' } = this.props
+      let floatText = ""
+      if (this.state.activeField) {
+         floatText = "field-active"
+      }
+      return (
+         <div>
+            <label htmlFor={input.name} className={floatText}>{label}</label>
+            <input {...input} type={type} id={input.name} label={label} className={className} onFocus={this.floatInputLabel} onBlur={this.landInputLabel} />
+            <p>{touched && error}</p>
+         </div>
+      )
+   }
+}
+
+export default Input
+
+
+
+
