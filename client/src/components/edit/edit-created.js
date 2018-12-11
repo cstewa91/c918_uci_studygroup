3b6f247ceb17bebb6376840aabdf218c3d058eff
Calberk <park.edmund@gmail.com>
2018-12-11 13:39:20 -0800

    added additional formatting to edit created page

--- client/src/components/edit/edit-created.js
+++ client/src/components/edit/edit-created.js
@@ -8,7 +8,8 @@ import Hamburger from '../general/hamburger';
 import Backdrop from '../general/backdrop';
 import { Field, reduxForm } from 'redux-form'
 import Input from '../input';
-
+import {Link} from "react-router-dom"
+import { createVerify } from 'crypto';
 
 
 
@@ -19,7 +20,6 @@ class EditGroup extends Component{
 
     toggleHamburger = () =>{
         this.setState((prevState) =>{
-            console.log(prevState)
             return {
                 hamburgerOpen: !prevState.hamburgerOpen
             }
@@ -42,7 +42,7 @@ class EditGroup extends Component{
     }
 
     render(){
-
+        console.log(this.props)
         let backdrop;
 
         if(this.state.hamburgerOpen){
@@ -56,36 +56,53 @@ class EditGroup extends Component{
                 <Header hamburgerClick = {this.toggleHamburger}/>  
                 <Hamburger show={this.state.hamburgerOpen}/>
                 {backdrop}  
-                <main className='main-content container'>
-                    <form className='edit-form row justify-content-center' onSubmit={handleSubmit(this.handleUpdateItem)}>
-                        <div className='edit-groupName col-8'>  
-                            <Field name="name" label="Group Name" component={Input} />
+                <main className='edit-content container'>
+                    <div className='edit-header row justify-content-center'>
+                        <p className='edit-title col-10'>EDIT</p>
+                        <Link to={`/group-info/${this.props.match.params.group_id}`} className='edit-return'>X</Link>
+                    </div>
+                    <form className='edit-form justify-content-center' onSubmit={handleSubmit(this.handleUpdateItem)}>
+                        <div className="row justify-content-between">
+                            <div className='edit-groupName col-7'>  
+                                <Field name="name" label="Group Name" component={Input} />
+                            </div>
+                            <div className='edit-groupSize col-3'>
+                                <Field name="max_group_size" label="Group Size" component={Input} />
+                            </div>
                         </div>
-                        <div className='edit-groupSize col-4'>
-                            <Field name="max_group_size" label="Group Size" component={Input} />
+                        <div className="row justify-content-between">
+                            <div className='edit-subject col-5'>
+                                <Field name="subject" label="Subject" component={Input} />
+                            </div>
+                            <div className='edit-course col-5'>
+                                <Field name="course" label="Course Number" component={Input} />
+                            </div>
                         </div>
-                        <div className='edit-subject col-4'>
-                            <Field name="subject" label="Subject" component={Input} />
+                        
+                        <div className="row justify-content-between">
+                            <div className='edit-startTime col-5'>
+                                <Field name="start_time" label="Starting Time" component={Input} type='datetime-local'/>
+                            </div>
+                            <div className='edit-endTime col-5'>
+                                <Field name="end_time" label="Ending Time" component={Input} type="datetime-local" />
+                            </div>
                         </div>
-                        <div className='edit-course col-4'>
-                            <Field name="course" label="Course Number" component={Input} />
+                        <div className="row justify-content-center">
+                            <div className='edit-location col-10'>
+                                <Field name="location" label="Location" component={Input} />
+                            </div>
                         </div>
-
-                        <div className='edit-startTime col-8'>
-                            <Field name="start_time" label="Starting Time" component={Input} type='datetime-local'/>
+                        <div className="row justify-content-center">
+                            <div className='edit-description col-10'>
+                                <Field name="description" label="Description" component={Input} />
+                            </div>
                         </div>
-                        <div className='edit-endTime col-8'>
-                            <Field name="end_time" label="Ending Time" component={Input} type="datetime-local" />
+                        <div className="row justify-content-center">
+                            <div className='edit-update col-8'>
+                                <button className='btn edit-update-button'>update</button>
+                            </div>  
                         </div>
-                        <div className='edit-location col-8'>
-                            <Field name="location" label="Location" component={Input} />
-                        </div>
-                        <div className='edit-description col-8'>
-                            <Field name="description" label="Description" component={Input} />
-                        </div>
-                        <div className='edit-update col-8'>
-                            <button className='btn edit-update-button'>update</button>
-                        </div>
+                        
                     </form>
                 </main>
             </div>
@@ -93,9 +110,31 @@ class EditGroup extends Component{
         }
 }
 
+function validate(values){
+    console.log('formvalues', values)
+    const {username, firstname, lastname, email} = values;
+    const error = {}
+
+    if(!username){
+        error.username = 'Please enter a username'
+    }
+    if(!firstname){
+        error.firstname = 'Please enter your first name'
+    }
+    if(!lastname){
+        error.lastname = 'Please enter your last name'
+    }
+    if(!email){
+        error.email = 'Please enter a valid email address'
+    }
+    return error
+}
+
+
 EditGroup = reduxForm({
     form: 'edit-group',
-    enableReinitialize: true
+    enableReinitialize: true,
+    validate: validate
 })(EditGroup)
 
 function mapStateToProps(state){
