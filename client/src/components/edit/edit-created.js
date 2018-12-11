3224676637f4099b0d4a68f75a50391eae076e58
Calberk <park.edmund@gmail.com>
2018-12-11 12:35:23 -0800

    added css to edit group page

--- client/src/components/edit/edit-created.js
+++ client/src/components/edit/edit-created.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { getGroupDetails } from '../../actions'
 import { editGroupInfo } from '../../actions'
@@ -9,7 +8,6 @@ import Hamburger from '../general/hamburger';
 import Backdrop from '../general/backdrop';
 import { Field, reduxForm } from 'redux-form'
 import Input from '../input';
-import NavButton from '../general/nav-button'
 
 
 
@@ -58,40 +56,38 @@ class EditGroup extends Component{
                 <Header hamburgerClick = {this.toggleHamburger}/>  
                 <Hamburger show={this.state.hamburgerOpen}/>
                 {backdrop}  
-                <main className='main-content'>
-                    <div className='container'>
-                    <p className='edit-group'>Edit Group:</p>
-                    <form onSubmit={handleSubmit(this.handleUpdateItem)}>
-                        <div>
+                <main className='main-content container'>
+                    <form className='edit-form row justify-content-center' onSubmit={handleSubmit(this.handleUpdateItem)}>
+                        <div className='edit-groupName col-8'>  
                             <Field name="name" label="Group Name" component={Input} />
                         </div>
-                        <div>
-                            <Field name="subject" label="Subject" component={Input} />
-                        </div>
-                        <div>
-                            <Field name="course" label="Course Number" component={Input} />
-                        </div>
-                        <div>
+                        <div className='edit-groupSize col-4'>
                             <Field name="max_group_size" label="Group Size" component={Input} />
                         </div>
-                        <div>
+                        <div className='edit-subject col-4'>
+                            <Field name="subject" label="Subject" component={Input} />
+                        </div>
+                        <div className='edit-course col-4'>
+                            <Field name="course" label="Course Number" component={Input} />
+                        </div>
+
+                        <div className='edit-startTime col-8'>
                             <Field name="start_time" label="Starting Time" component={Input} type='datetime-local'/>
                         </div>
-                        <div>
+                        <div className='edit-endTime col-8'>
                             <Field name="end_time" label="Ending Time" component={Input} type="datetime-local" />
                         </div>
-                        <div>
+                        <div className='edit-location col-8'>
                             <Field name="location" label="Location" component={Input} />
                         </div>
-                        <div>
+                        <div className='edit-description col-8'>
                             <Field name="description" label="Description" component={Input} />
                         </div>
-                        <button>update</button>
-                        {/* <NavButton to='' text='UPDATE' /> */}
+                        <div className='edit-update col-8'>
+                            <button className='btn edit-update-button'>update</button>
+                        </div>
                     </form>
-                    </div>
                 </main>
-               
             </div>
                 )
         }
