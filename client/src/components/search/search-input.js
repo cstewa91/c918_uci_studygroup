2aabea5d9a5f4deb5253813738d662db68a5eca5
Erin Tait <erinmtait@gmail.com>
2019-01-16 09:21:14 -0800

    changed filter button to search and adjusted css at smaller screen sizes

--- client/src/components/search/search-input.js
+++ client/src/components/search/search-input.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default ({ props, resetFilter, input, className, label, meta: size, type = 'text' }) => (
    <div>
-      <input {...input} type={type} placeholder={label} size={size} className={className}/> <button className="search-filter-button" type="submit">FILTER</button> <button className="reset-search-button" onClick={resetFilter} type="button">RESET</button>
+      <input {...input} type={type} placeholder={label} size={size} className={className}/> <button className="search-filter-button" type="submit">SEARCH</button> <button className="reset-search-button" onClick={resetFilter} type="button">RESET</button>
    </div>
 )
